62c2c6807fba7fc129a919176c86e80a1c9989ec
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-02-22 09:55:25 -0700

    refactor: remove welcome message from SiteLayout component

--- src/layouts/SiteLayout/SiteLayout.tsx
+++ src/layouts/SiteLayout/SiteLayout.tsx
@@ -13,11 +13,6 @@ export function SiteLayout({ children }: SiteLayoutProps) {
     <div className='flex flex-col min-h-screen'>
       <Navigation />
       <main className='flex-1'>
-        <div className='max-w-6xl mx-auto px-4 py-8'>
-          <h1 className='text-4xl font-bold mb-8'>
-            Hello! Welcome to TerenceWaters.com
-          </h1>
-        </div>
         {children}
       </main>
       <Footer />
