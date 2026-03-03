57b793f802285fbaa89478d9b6f6196fd3ee21d6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-03-02 19:54:54 -0700

    Fix formatting in test function response headers and body

--- api/test/index.js
+++ api/test/index.js
@@ -1,11 +1,11 @@
 module.exports = async function (req, context) {
   context.log('Test function triggered');
-  
+
   return {
     status: 200,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ message: 'Test function works!' })
+    body: JSON.stringify({ message: 'Test function works!' }),
   };
 };
