8fa5a786d1b0ebaa9d602400766ec44da7fcd3e6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-03-23 13:07:39 -0600

    Fix layout width for left-handed and stacked layouts in About and Standard Page components

--- src/app/about/AboutPageClient.tsx
+++ src/app/about/AboutPageClient.tsx
@@ -43,14 +43,14 @@ export const AboutPageClient: React.FC<AboutPageClientProps> = ({
   const cardPaneClasses = useStackedLayout
     ? 'flex items-center justify-center pt-4 md:pt-8'
     : isLeftHanded
-      ? 'flex items-center justify-center pt-16 md:pt-0 md:fixed md:right-0 md:top-16 md:bottom-0 md:w-1/2 lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
-      : 'flex items-center justify-center pt-16 md:pt-0 md:fixed md:left-0 md:top-16 md:bottom-0 md:w-1/2 lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden';
+      ? 'flex items-center justify-center pt-16 md:pt-0 md:fixed md:right-0 md:top-16 md:bottom-0 md:w-[40%] lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
+      : 'flex items-center justify-center pt-16 md:pt-0 md:fixed md:left-0 md:top-16 md:bottom-0 md:w-[40%] lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden';
 
   const contentPaneClasses = useStackedLayout
     ? 'flex-1 flex flex-col'
     : isLeftHanded
-      ? 'flex-1 md:mr-[50%] lg:mr-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
-      : 'flex-1 md:ml-[50%] lg:ml-[33.333333%] md:h-full md:overflow-y-auto flex flex-col';
+      ? 'flex-1 md:mr-[40%] lg:mr-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
+      : 'flex-1 md:ml-[40%] lg:ml-[33.333333%] md:h-full md:overflow-y-auto flex flex-col';
 
   return (
     <SiteLayout showFooter={false}>
--- src/layouts/PageLayout/StandardPageLayout.tsx
+++ src/layouts/PageLayout/StandardPageLayout.tsx
@@ -68,16 +68,16 @@ export function StandardPageLayout({
       ? 'md:fixed md:right-0 md:top-16 md:bottom-0 md:w-1/4 xl:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
       : 'md:fixed md:left-0 md:top-16 md:bottom-0 md:w-1/4 xl:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
     : isLeftHanded
-      ? 'md:fixed md:right-0 md:top-16 md:bottom-0 md:w-1/2 lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
-      : 'md:fixed md:left-0 md:top-16 md:bottom-0 md:w-1/2 lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden';
+      ? 'md:fixed md:right-0 md:top-16 md:bottom-0 md:w-[40%] lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden'
+      : 'md:fixed md:left-0 md:top-16 md:bottom-0 md:w-[40%] lg:w-1/3 md:flex md:items-center md:justify-center md:p-4 md:overflow-hidden';
 
   const contentPaneClasses = isMobileLandscape
     ? isLeftHanded
       ? 'flex-1 md:mr-[25%] xl:mr-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
       : 'flex-1 md:ml-[25%] xl:ml-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
     : isLeftHanded
-      ? 'flex-1 md:mr-[50%] lg:mr-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
-      : 'flex-1 md:ml-[50%] lg:ml-[33.333333%] md:h-full md:overflow-y-auto flex flex-col';
+      ? 'flex-1 md:mr-[40%] lg:mr-[33.333333%] md:h-full md:overflow-y-auto flex flex-col'
+      : 'flex-1 md:ml-[40%] lg:ml-[33.333333%] md:h-full md:overflow-y-auto flex flex-col';
 
   // Contained viewport layout with feature image
   if (featureImage) {
