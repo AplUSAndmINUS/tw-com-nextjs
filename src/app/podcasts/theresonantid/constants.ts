33785abd30692ce6022694dfaa74f916f7ff96f6
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2026-07-15 08:48:11 -0600

    content: update Instagram and about links for The Resonant Identity

--- src/app/podcasts/theresonantid/constants.ts
+++ src/app/podcasts/theresonantid/constants.ts
@@ -1,9 +1,9 @@
 export const TRI_LINKS = {
-  instagram: 'https://www.instagram.com/theresonantid',
+  instagram: 'https://www.instagram.com/theresonantidentity/',
   twitter: 'https://x.com/theresonantid',
   tiktok: 'https://www.tiktok.com/@theresonantid',
   podcast: '/podcasts/theresonantid/', // setting as the main podcast page for now since it links to all platforms
-  about: 'https://fluxline.pro/podcasts/theresonantid/about',
+  about: 'https://www.theresonantidentity.com/',
   website: 'https://www.theresonantidentity.com/',
   facebookGroup: 'https://www.facebook.com/groups/theresonantidentity',
   facebookPage: 'https://www.facebook.com/theresonantidentity',
